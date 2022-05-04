9be7dfb3e18776f76d836cc7585efd0f88c4f465
HectorTorresE <hectorjte@gmail.com>
2022-05-04 14:27:50 -0600

    Fix linter error

--- morse_code.rb
+++ morse_code.rb
@@ -1,13 +1,12 @@
 $code_pair = {
-  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', 
-  '.'  => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H', 
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+  '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
   '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-  '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', 
-  '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', 
-  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', 
-  '-.--' => 'Y', '--..' => 'Z' 
+  '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+  '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+  '-.--' => 'Y', '--..' => 'Z'
 }
-
 
 def decode_char(code)
   return $code_pair[code]
@@ -16,8 +15,8 @@ end
 def decode_word(word)
   @current_word = word.split(' ')
   @decoded_word = ''
-  @current_word.each do |letter|    
-   @decoded_word += decode_char(letter)
+  @current_word.each do |letter|
+    @decoded_word += decode_char(letter)
   end
   return @decoded_word;
 end
@@ -25,11 +24,10 @@ end
 def decode_sentence(sentence)
   @words = sentence.split('   ')
   @decoded = ''
-  @words.each do |word|    
-   @decoded += decode_word(word) + ' '
+  @words.each do |word|
+    @decoded += decode_word(word) + ' '
   end
   return @decoded;
 end
 
 puts decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-
