5243d7492bab9d4c9de134b6f4fd6c56b78fac7c
mengiefen <mengiefen@gmail.com>
2022-05-04 23:14:53 +0300

    add decode_word and decode_char funtions

---
+++ morse_code.rb
@@ -0,0 +1,24 @@
+$code_pair = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', 
+  '.'  => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H', 
+  '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', 
+  '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', 
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', 
+  '-.--' => 'Y', '--..' => 'Z' 
+}
+
+
+def decode_char(code)
+  return $code_pair[code]
+end
+
+def decode_word(word)
+  @current_word = word.split(' ')
+  @decoded_word = ''
+  @current_word.each do |letter|    
+   @decoded_word += decode_char(letter)
+  end
+  return @decoded_word;
+end
+
